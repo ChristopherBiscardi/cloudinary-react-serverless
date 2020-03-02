7c6d9b998f79dd814935be2b5d46263a497d3f8a
Christopher Biscardi <chris@christopherbiscardi.com>
2020-03-02 15:56:31 -0800

    asflkj

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -1,7 +1,7 @@
-const playwright = require("playwright-chromium");
+const playwright = require("playwright-aws-lambda");
 
 exports.handler = async function(event, ctx) {
-  const browser = await playwright.chromium.launch();
+  const browser = await playwright.launchChromium();
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("http://whatsmyuseragent.org/");
