2c796e3709ed8fc2faeeef33957857d974afd038
Christopher Biscardi <chris@christopherbiscardi.com>
2020-03-02 16:00:55 -0800

    chromium?

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -2,7 +2,7 @@ const playwright = require("playwright-aws-lambda");
 
 exports.handler = async function(event, ctx) {
   const browser = await playwright.launchChromium();
-  const context = await browser.newContext();
+  const context = await browser.defaultContext();
   const page = await context.newPage();
   await page.goto("http://whatsmyuseragent.org/");
   const screenshotBuffer = await page.screenshot();
